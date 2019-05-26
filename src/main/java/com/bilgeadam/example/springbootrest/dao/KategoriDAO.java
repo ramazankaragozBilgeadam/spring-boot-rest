89da8b3c1666903132044c171c69a91c1aab70f0
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-05-26 12:33:08 +0300

    entity

---
+++ src/main/java/com/bilgeadam/example/springbootrest/dao/KategoriDAO.java
@@ -0,0 +1,9 @@
+package com.bilgeadam.example.springbootrest.dao;
+
+import com.bilgeadam.example.springbootrest.entity.Kategori;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KategoriDAO extends JpaRepository<Kategori,Long> {
+}
