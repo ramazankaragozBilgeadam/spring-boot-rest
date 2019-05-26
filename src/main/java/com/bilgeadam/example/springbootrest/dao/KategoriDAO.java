ca72b2dd9b42ce5c0b7e5acc4c6e5cfa90bc4286
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-05-26 14:21:44 +0300

    rest

--- src/main/java/com/bilgeadam/example/springbootrest/dao/KategoriDAO.java
+++ src/main/java/com/bilgeadam/example/springbootrest/dao/KategoriDAO.java
@@ -4,6 +4,11 @@ import com.bilgeadam.example.springbootrest.entity.Kategori;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KategoriDAO extends JpaRepository<Kategori,Long> {
+
+
+    List<Kategori> findAllByKitap_Adi(String adi);
 }
